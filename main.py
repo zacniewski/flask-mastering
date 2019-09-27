3d251a0b1ebb630f477ee3390137db48b7ce6cee
Artur Zacniewski <a.zacniewski@gmail.com>
2019-09-27 22:08:14 +0200

    Configuration file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import DevConfig
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
 
 
 @app.route('/')
@@ -9,4 +11,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
